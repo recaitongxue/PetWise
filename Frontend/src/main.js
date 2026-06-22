08da925cc5903d931449fedd756f4804935ad386
minchyang107-jpg <minchyang107@gmail.com>
2026-06-22 13:08:40 +0800

    Update main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './styles/theme.css'  //额外引入 ./styles/theme.css
 import App from './App.vue'
 import router from './router'
 import store from './store'
